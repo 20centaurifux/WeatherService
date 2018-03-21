8ed351e86383c646dfb2a47e114f75d971c93ea0
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2018-03-21 16:09:25 +0100

    enable logout

---
+++ WeatherService/Controllers/LogoutController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WeatherService.Models;
+
+namespace WeatherService.Controllers
+{
+    public class LogoutController : Controller
+    {
+        SignInManager<User> _signInManager;
+
+        public LogoutController(SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                _signInManager.SignOutAsync();
+            }
+
+            return Redirect("/Home");
+        }
+    }
+}
