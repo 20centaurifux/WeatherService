6073fa06922a8d8fd6cce3a2d8656f95d50478c2
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2018-03-22 21:47:27 +0100

    station management screen with delete function

--- WeatherService/Controllers/WeatherStationsController.cs
+++ WeatherService/Controllers/WeatherStationsController.cs
@@ -2,11 +2,39 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using WeatherService.Data;
+using LinqToDB;
 
 namespace WeatherService.Controllers
 {
     public class WeatherStationsController : Controller
     {
+        [HttpGet]
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Index()
+        {
+            using (var db = new WeatherDb())
+            {
+                var q = from s in db.WeatherStation
+                        select s;
+
+                return View(q.ToArray());
+            }
+        }
+
+        [HttpDelete("WeatherStations/{id?}")]
+        [Authorize(Roles = "Administrator")]
+        public IActionResult Delete(string id)
+        {
+            using (var db = new WeatherDb())
+            {
+                db.WeatherStation
+                    .Where(s => s.Id.Equals(id))
+                    .Delete();
+
+                return Ok();
+            }
+        }
+
         [HttpGet]
         [AllowAnonymous]
         public IActionResult Available()
