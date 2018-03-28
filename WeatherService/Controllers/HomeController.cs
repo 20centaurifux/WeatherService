968fc28150b6a6eb0b2f1be25d110b42b4770293
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2018-03-28 16:48:48 +0200

    add dialog for adding widgets on home screen, use gridster.js to display widgets

--- WeatherService/Controllers/HomeController.cs
+++ WeatherService/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WeatherService.Data;
+using WeatherService.Models;
 
 namespace WeatherService.Controllers
 {
@@ -14,7 +17,27 @@ namespace WeatherService.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var p = WidgetProvider.FromHostingEnvironment(_env);
+
+            var m = new Dashboard()
+            {
+                AvailableWidgets = p.LoadWidgets(),
+                SupportedStations = new Dictionary<System.Guid, System.Collections.Generic.IEnumerable<WeatherStation>>()
+            };
+
+            foreach(var w in m.AvailableWidgets)
+            {
+                if(User.Identity.IsAuthenticated)
+                {
+                    m.SupportedStations.Add(w.Guid, p.GetSupportedStations(w));
+                }
+                else
+                {
+                    m.SupportedStations.Add(w.Guid, p.GetSupportedPublicStations(w));
+                }
+            }
+
+            return View(m);
         }
     }
 }
--- WeatherService/Models/Dashboard.cs
+++ WeatherService/Models/Dashboard.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace WeatherService.Models
 {
     public class Dashboard
     {
         public IEnumerable<Widget> AvailableWidgets { get; set; }
+        public Dictionary<Guid, IEnumerable<WeatherStation>> SupportedStations { get; set; }
     }
 }
