54904f7ae44cd7d9a17e4a7e20491cc45c370972
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2018-04-01 19:47:18 +0200

    pressure widget

--- WeatherService/Controllers/WidgetsController.cs
+++ WeatherService/Controllers/WidgetsController.cs
@@ -11,6 +11,8 @@ namespace WeatherService.Controllers
 {
     public class WidgetsController : Controller
     {
+        public static readonly int CURRENT_LOG_ENTRY_HOURS_FILTER = 3;
+
         [Security.Filters.Widget(Guid = "c72fe6ee-4f94-4732-be36-34b2a1f8f370")]
         public IActionResult Temperature()
         {
@@ -45,13 +47,36 @@ namespace WeatherService.Controllers
             return View(m);
         }
 
+        [Security.Filters.Widget(Guid = "15fe2e9c-53b6-44e4-afb2-1d3b417b93d5")]
+        public IActionResult Pressure()
+        {
+            var station = GetStation();
+
+            using (var db = new WeatherDb())
+            {
+                var start = Utils.DateTimeConverter.BeginningOfDay(DateTime.UtcNow);
+                var end = Utils.DateTimeConverter.EndOfDay(start);
+
+                var m = new LogEntries()
+                {
+                    StationName = station.Name,
+                    Entries = db.LogEntry.Where(l => l.StationId.Equals(station.Id) && l.Timestamp >= start && l.Timestamp <= end).ToArray()
+                };
+
+                return View(m);
+            };
+        }
+
         [Security.Filters.Widget(Guid = "8e0ce9be-ce79-4ace-9159-a8a158694fa5")]
         public IActionResult WebcamImage()
         {
             var station = GetStation();
             var gen = new RandomGenerator();
+            var url = gen.AppendParameterToUrl(station.WebcamUrl);
 
-            return View(CreateSingleStationValue<string>(station, DateTime.UtcNow, gen.AppendParameterToUrl(station.WebcamUrl)));
+            var m = CreateSingleStationValue<string>(station, DateTime.UtcNow, url);
+
+            return View(m);
         }
 
         private IEnumerable<WeatherStation> GetStations()
@@ -76,7 +101,7 @@ namespace WeatherService.Controllers
             {
                 var entry = db.LogEntry.Where(l => l.StationId.Equals(station.Id)).OrderByDescending(l => l.Timestamp).FirstOrDefault();
 
-                if (entry != null && DateTime.UtcNow.Subtract(entry.Timestamp).TotalHours < 5)
+                if (entry != null && DateTime.UtcNow.Subtract(entry.Timestamp).TotalHours <= CURRENT_LOG_ENTRY_HOURS_FILTER)
                 {
                     return entry;
                 }
---
+++ WeatherService/Models/Widgets/LogEntries.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeatherService.Models.Widgets
+{
+    public class LogEntries
+    {
+        public string StationName { get; set; }
+        public IEnumerable<LogEntry> Entries { get; set; }
+    }
+}
